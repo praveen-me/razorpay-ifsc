e035a3101594cc2f49b0ee61f7d4a44baa0ca6da
praveen-me <praveenkrsaini0@gmail.com>
2019-01-21 21:08:38 +0530

    Fix issue wih webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 /* eslint-disable */
 var webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
@@ -66,7 +65,6 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: "bundle.css",
-    }),
-    new HtmlWebpackPlugin()
+    })
   ]
 }
