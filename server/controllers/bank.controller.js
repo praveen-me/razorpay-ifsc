0b59b0abb2334c92bfabbeab5a191edc5d5f3711
praveen-me <praveenkrsaini0@gmail.com>
2019-01-21 20:47:47 +0530

    Added Working Files

---
+++ server/controllers/bank.controller.js
@@ -0,0 +1,20 @@
+const Bank = require('./../models/Bank');
+
+module.exports = {
+  setBank: (req) => {
+    const { IFSC, BANK, CITY } = req.body;
+    Bank.findOne({ IFSC }, (err, bankData) => {
+      if (!bankData) {
+        const newBank = new Bank({
+          IFSC,
+          BANK,
+          CITY,
+        });
+        return newBank.save();
+      }
+      return {
+        msg: 'Bank information already available',
+      };
+    });
+  },
+};
